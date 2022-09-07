08dc18df94e7afeba95c19928c38d38fcdb60f9f
daniel <danielotavianopontes@gmail.com>
2022-09-06 23:01:15 -0300

    fix land function

--- fun/aula3/Av2.hs
+++ fun/aula3/Av2.hs
@@ -1,12 +1,12 @@
 module Av where
 
-data Boolean = True' | False'  deriving (Show)
+data Boolean = True' | False' deriving (Show)
 
 lor :: Boolean -> Boolean -> Boolean
 lor True' _ = True'
 lor _ True' = True'
 lor _ _ = False'
 
-land :: Boolean -> Boolean 
+land :: Boolean -> Boolean -> Boolean
 land True' True' = True'
 land _ _ = False'
