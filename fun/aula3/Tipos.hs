e81823213b0ecc8be0ff08801ce39828e38fe77f
daniel <danielotavianopontes@gmail.com>
2022-09-06 22:59:44 -0300

    add Tipos example

--- fun/aula3/Tipos.hs
+++ fun/aula3/Tipos.hs
@@ -1,2 +1,18 @@
 module Tipos where
 
+data Weekday = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+  deriving (Show)
+
+nextDay :: Weekday -> Weekday
+nextDay Monday = Tuesday
+nextDay Tuesday = Wednesday
+nextDay Wednesday = Thursday
+nextDay Thursday = Friday
+nextDay Friday = Saturday
+nextDay Saturday = Sunday
+nextDay Sunday = Monday
+
+nextWorkingDay :: Weekday -> Weekday
+nextWorkingDay Friday = Monday
+nextWorkingDay Saturday = Monday
+nextWorkingDay day = nextDay day
