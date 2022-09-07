ac9a1337f6f8fb7e476f1d31a2f6e5b4eb5dc059
daniel <danielotavianopontes@gmail.com>
2022-09-06 23:51:01 -0300

    improve Weekday data

--- fun/aula3/Tipos.hs
+++ fun/aula3/Tipos.hs
@@ -1,7 +1,27 @@
 module Tipos where
 
 data Weekday = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
-  deriving (Show)
+
+--  deriving (Show, Eq)
+
+instance (Eq Weekday) where
+  Monday == Monday = True
+  Tuesday == Tuesday = True
+  Wednesday == Wednesday = True
+  Thursday == Thursday = True
+  Friday == Friday = True
+  Saturday == Saturday = True
+  Sunday == Sunday = True
+  _ == _ = False
+
+instance (Show Weekday) where
+  show Monday = "Monday"
+  show Tuesday = "Tuesday"
+  show Wednesday = "Wednesday"
+  show Thursday = "Thursday"
+  show Friday = "Friday"
+  show Saturday = "Saturday"
+  show Sunday = "Sunday"
 
 nextDay :: Weekday -> Weekday
 nextDay Monday = Tuesday
