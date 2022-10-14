cf0802a11989de92fe7e27ea505f1135568f8153
daniel <danielotavianopontes@gmail.com>
2022-10-14 16:49:03 -0300

    ExRat

--- ex/ExRat.hs
+++ ex/ExRat.hs
@@ -7,35 +7,39 @@ module ExRat
 
 -- define Rat:
 data Rat = Rat Integer Integer
-  deriving Show
 
--- instance Show Rat where
---     show = undefined
+instance Show Rat where
+  show (Rat i1 i2) = show i1 ++ " / " ++ show i2
 
 instance Eq Rat where
   Rat x y == Rat n m = x * m == y * n
 
 instance Num Rat where
-  (+)         = undefined
-  (*)         = undefined
-  negate      = undefined
-  abs         = undefined
-  signum      = undefined
-  fromInteger = undefined
+  (+) (Rat x y) (Rat n m) = Rat (x * m + n * y) (y * m)
+  (*) (Rat x y) (Rat n m) = Rat (x * n) (y * m)
+  negate (Rat x y) = Rat (-x) y
+  abs (Rat x y) | x < 0     = Rat (-x) y
+                | otherwise = Rat x y
+  signum (Rat x _) | x == 0    = Rat 0 1
+                   | x < 0     = Rat (-1) 1
+                   | otherwise = Rat 1 1
+  fromInteger x = Rat x 1
 
 instance Ord Rat where
-  compare = undefined
+  compare r1 r2 | r1 == r2  = EQ
+                | r1 < r2   = LT
+                | otherwise = GT
 
 rat :: Integer -> Integer -> Rat
 rat _ 0 = error "denominator is zero"
 rat x y = Rat x y
 
 (//) :: Rat -> Rat -> Rat
-(//) = undefined
+(//) r (Rat n m) = r * Rat m n
 
 denominator :: Rat -> Integer
-denominator = undefined
+denominator (Rat _ m) = m
 
 numerator :: Rat -> Integer
-numerator = undefined
+numerator (Rat n _) = n
 
