ff2b1825d64223b35277be49dce6f9698e842a5d
daniel <danielotavianopontes@gmail.com>
2022-10-14 10:21:33 -0300

    ExMaybe v1

--- ex/ExMaybe.hs
+++ ex/ExMaybe.hs
@@ -1,39 +1,55 @@
 module ExMaybe where
 
+import qualified Data.Maybe                    as M
 -- Do not alter this import!
-import Prelude hiding ( maybe, Maybe(..) )
-import qualified Data.Maybe as M
+import           Prelude                 hiding ( Maybe(..)
+                                                , maybe
+                                                )
 
 data Maybe a = Nothing | Just a
     deriving (Show, Eq, Ord)
 
 catMaybes :: [Maybe a] -> [a]
-catMaybes = undefined
+catMaybes []              = []
+catMaybes (Nothing  : xs) = catMaybes xs
+catMaybes ((Just x) : xs) = x : catMaybes xs
+
 
 fromJust :: Maybe a -> a
-fromJust = undefined
+fromJust (Just x) = x
+fromJust _        = error "Nothing"
 
 fromMaybe :: a -> Maybe a -> a
-fromMaybe = undefined
+fromMaybe _ (Just x) = x
+fromMaybe d Nothing  = d
 
 isJust :: Maybe a -> Bool
-isJust = undefined
+isJust (Just _) = True
+isJust _        = False
 
 isNothing :: Maybe a -> Bool
-isNothing = undefined
+isNothing Nothing = True
+isNothing _       = False
 
 listToMaybe :: [a] -> Maybe a
 listToMaybe = undefined
 
 mapMaybe :: (a -> Maybe b) -> [a] -> [b]
-mapMaybe = undefined
+mapMaybe _ [] = []
+mapMaybe p (x : xs) | isJust result = fromJust result : mapMaybe p xs
+                    | otherwise     = mapMaybe p xs
+  where result = p x
 
 maybe :: b -> (a -> b) -> Maybe a -> b
-maybe = undefined
+maybe d p Nothing  = d
+maybe _ p (Just x) = p x
 
 maybeToList :: Maybe a -> [a]
-maybeToList = undefined
+maybeToList Nothing  = []
+maybeToList (Just x) = [x]
 
 tryToModifyWith :: [Maybe (a -> a)] -> [a] -> [a]
-tryToModifyWith = undefined
-
+tryToModifyWith []              _        = []
+tryToModifyWith _               []       = []
+tryToModifyWith (Nothing  : fs) (x : xs) = tryToModifyWith fs xs
+tryToModifyWith ((Just f) : fs) (x : xs) = f x : tryToModifyWith fs xs
